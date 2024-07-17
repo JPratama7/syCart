f842557f2f0c99f8cf22722842b2db2a09a24f46
JPratama7 <josepratama080@gmail.com>
2024-07-17 23:53:25 +0700

    feat: cart with product data

--- model/model.go
+++ model/model.go
@@ -4,6 +4,13 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Status string
+
+const (
+	PENDING Status = "PENDING"
+	SUCCESS Status = "SUCCESS"
+)
+
 type Token struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
@@ -45,11 +52,20 @@ type CartItem struct {
 	AddedAt    primitive.DateTime `json:"added_at" bson:"added_at"`
 }
 
+type CartItemWithProduct struct {
+	CartItemId primitive.ObjectID `json:"cart_item_id" bson:"_id,omitempty"`
+	UserId     primitive.ObjectID `json:"user_id" bson:"user_id"`
+	ProductId  primitive.ObjectID `json:"product_id" bson:"product_id"`
+	Quantity   int                `json:"quantity" bson:"quantity"`
+	AddedAt    primitive.DateTime `json:"added_at" bson:"added_at"`
+	Product    Product            `bson:"product"`
+}
+
 type Order struct {
 	OrderId     primitive.ObjectID `json:"order_id" bson:"_id,omitempty"`
 	UserId      primitive.ObjectID `json:"user_id" bson:"user_id"`
 	TotalAmount float64            `json:"total_amount" bson:"total_amount"`
-	Status      string             `json:"status" bson:"status"`
+	Status      Status             `json:"status" bson:"status"`
 	OrderItems  *[]OrderItem       `json:"order_items" bson:"order_items"`
 	CreatedAt   primitive.DateTime `json:"created_at" bson:"created_at"`
 	UpdatedAt   primitive.DateTime `json:"updated_at" bson:"updated_at"`
@@ -65,7 +81,7 @@ type Payment struct {
 	PaymentId     primitive.ObjectID `json:"payment_id" bson:"_id,omitempty"`
 	OrderId       primitive.ObjectID `json:"order_id" bson:"order_id"`
 	Amount        float64            `json:"amount" bson:"amount"`
-	Status        string             `json:"status" bson:"status"`
+	Status        Status             `json:"status" bson:"status"`
 	TransactionId [12]byte           `json:"transaction_id" bson:"transaction_id"`
 	CreatedAt     primitive.DateTime `json:"created_at" bson:"created_at"`
 }
