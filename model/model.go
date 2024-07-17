67031e49d147fa23af57032c3bca9fd9391a0bd2
JPratama7 <josepratama080@gmail.com>
2024-07-17 15:49:28 +0700

    refactor: change id field to _id

--- model/model.go
+++ model/model.go
@@ -5,7 +5,7 @@ import (
 )
 
 type User struct {
-	UserId       primitive.ObjectID  `json:"user_id" bson:"user_id,omitempty"`
+	UserId       primitive.ObjectID  `json:"user_id" bson:"_id,omitempty"`
 	Username     string              `json:"username" bson:"username"`
 	Email        string              `json:"email" bson:"email"`
 	PasswordHash string              `json:"-" bson:"passwordHash"`
@@ -14,7 +14,7 @@ type User struct {
 }
 
 type Product struct {
-	ProductId     primitive.ObjectID  `json:"product_id" bson:"product_id,omitempty"`
+	ProductId     primitive.ObjectID  `json:"product_id" bson:"_id,omitempty"`
 	CategoryId    int                 `json:"category_id" bson:"category_id"`
 	Name          string              `json:"name" bson:"name"`
 	Description   string              `json:"description" bson:"description"`
@@ -26,13 +26,13 @@ type Product struct {
 }
 
 type Category struct {
-	CategoryId  primitive.ObjectID `json:"category_id" bson:"category_id,omitempty"`
+	CategoryId  primitive.ObjectID `json:"category_id" bson:"_id,omitempty"`
 	Name        string             `json:"name" bson:"name"`
 	Description string             `json:"description" bson:"description"`
 }
 
 type CartItem struct {
-	CartItemId primitive.ObjectID  `json:"cart_item_id" bson:"cart_item_id,omitempty"`
+	CartItemId primitive.ObjectID  `json:"cart_item_id" bson:"_id,omitempty"`
 	UserId     primitive.ObjectID  `json:"user_id" bson:"user_id"`
 	ProductId  primitive.ObjectID  `json:"product_id" bson:"product_id"`
 	Quantity   int                 `json:"quantity" bson:"quantity"`
@@ -40,7 +40,7 @@ type CartItem struct {
 }
 
 type Order struct {
-	OrderId     primitive.ObjectID  `json:"order_id" bson:"order_id,omitempty"`
+	OrderId     primitive.ObjectID  `json:"order_id" bson:"_id,omitempty"`
 	UserId      primitive.ObjectID  `json:"user_id" bson:"user_id"`
 	TotalAmount float64             `json:"total_amount" bson:"total_amount"`
 	Status      string              `json:"status" bson:"status"`
@@ -50,16 +50,16 @@ type Order struct {
 }
 
 type OrderItem struct {
-	ProductId       primitive.ObjectID `json:"product_id" bson:"product_id,omitempty"`
+	ProductId       primitive.ObjectID `json:"product_id" bson:"_id,omitempty"`
 	Quantity        int                `json:"quantity" bson:"quantity"`
 	PriceAtPurchase float64            `json:"price_at_purchase" bson:"price_at_purchase"`
 }
 
 type Payment struct {
-	PaymentId     primitive.ObjectID  `json:"payment_id" bson:"payment_id,omitempty"`
+	PaymentId     primitive.ObjectID  `json:"payment_id" bson:"_id,omitempty"`
 	OrderId       primitive.ObjectID  `json:"order_id" bson:"order_id"`
 	Amount        float64             `json:"amount" bson:"amount"`
 	Status        string              `json:"status" bson:"status"`
-	TransactionId string              `json:"transaction_id" bson:"transaction_id"`
+	TransactionId [12]byte            `json:"transaction_id" bson:"transaction_id"`
 	CreatedAt     primitive.Timestamp `json:"created_at" bson:"created_at"`
 }
