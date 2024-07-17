a2043f64bda796c28a388c0b821776400279d318
JPratama7 <josepratama080@gmail.com>
2024-07-17 18:21:31 +0700

    feat: validate parameter

--- model/request/req.go
+++ model/request/req.go
@@ -1,13 +1,13 @@
 package request
 
 type Register struct {
-	Email    string `json:"email,required"`
-	Username string `json:"username,required"`
-	Password string `json:"password,required"`
+	Email    string `json:"email" validate:"required"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
 }
 
 type Login struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
-	Password string `json:"password,required"`
+	Password string `json:"password,required" validate:"required"`
 }
